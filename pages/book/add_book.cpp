82b309eb805d45668fbdafe0a5993c9b9ce5376c
i25033741-wq <i25033741@student.newinti.edu.my>
2025-11-03 22:47:50 +0800

    Update add_book.cpp
    111

--- pages/book/add_book.cpp
+++ pages/book/add_book.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void addBook() {
    // 在这里写logic
    cout << "\nPress Enter to return to menu...";
-   cout << "\nChange by JY" << endl;
+   cout << "\nChange by adrian" << endl;
    cin.ignore();
    cin.get();  // Wait for user input before returning
 }

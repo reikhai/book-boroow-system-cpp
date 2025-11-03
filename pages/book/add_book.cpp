60903527159d93c8842372ae90ef079a924d7951
jy899 <150579683+jy899@users.noreply.github.com>
2025-11-03 22:08:58 +0800

    Update add_book.cpp
    Hi, here is the new amend by JY

--- pages/book/add_book.cpp
+++ pages/book/add_book.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void addBook() {
    // 在这里写logic
    cout << "\nPress Enter to return to menu...";
-   cout << "\nHello from addBook function!" << endl;
+   cout << "\nChange by JY" << endl;
    cin.ignore();
    cin.get();  // Wait for user input before returning
 }
